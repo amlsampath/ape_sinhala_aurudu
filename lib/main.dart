1f4d38853dbd2d55cc09a101695811c088cf6552
lahiru13 <lahiru@yukon.lk>
2025-04-03 19:50:45 +0530

    connect with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:ape_sinhala_aurudu/pages/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:ape_sinhala_aurudu/pages/welcome_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   final prefs = await SharedPreferences.getInstance();
   final hasSeenWelcome = prefs.getBool('hasSeenWelcome') ?? false;
   runApp(MyApp(hasSeenWelcome: hasSeenWelcome));
